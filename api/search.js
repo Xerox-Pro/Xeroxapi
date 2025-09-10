c67129c90ffb95f090cbe22156e0b8b8a0e3805c
Xerox. <oz.ozk.takeru0000@gmail.com>
2025-09-10 17:44:17 +0900

    Update search.js

--- api/search.js
+++ api/search.js
@@ -11,7 +11,12 @@ export default async function handler(req, res) {
 
     const limit = parseInt(req.query.limit) || 50;
 
-    const searchResult = await youtube.search(query, { type: "video" });
+    // 検索フィルターをクエリから取得
+    const filter = req.query.filter || ""; // 例: "video,short,4k,recent"
+    // 分割して配列にする（空文字なら空配列）
+    const filters = filter ? filter.split(",") : [];
+
+    const searchResult = await youtube.search(query, { type: "video", filters });
     let results = searchResult.videos ? Array.from(searchResult.videos) : [];
 
     while (results.length < limit && searchResult.has_continuation) {
