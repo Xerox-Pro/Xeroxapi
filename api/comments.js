b2330b86ae714d5a148b8393f41de7f11acde047
Xerox. <oz.ozk.takeru0000@gmail.com>
2025-09-10 22:01:28 +0900

    Update comments.js

--- api/comments.js
+++ api/comments.js
@@ -21,22 +21,11 @@ export default async function handler(req, res) {
 
     res.json({
       comments: allComments.slice(0, limit).map(c => ({
-        comment_id: c.comment_id || null,
-        text: c.content?.text || "",
-        published_time: c.published_time || "",
-        author: {
-          id: c.author?.id || null,
-          name: c.author?.name || "",
-          thumbnails: c.author?.thumbnails?.map(t => t.url) || [],
-        },
-        is_member: c.is_member || false,
-        member_badge: c.member_badge?.url || null,
-        like_count: c.vote_count || 0,
-        reply_count: c.reply_count || 0,
-        is_pinned: c.is_pinned || false
+        author: c.author?.name,
+        text: c.content?.text,
+        likes: c.vote_count
       }))
     });
-
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
